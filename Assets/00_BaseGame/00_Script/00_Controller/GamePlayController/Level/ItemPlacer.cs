4b5a90dd0408a39fc5ab9178ef7d89a14fa2da0c
Dung <vuquocdung204@gmail.com>
2025-09-03 18:39:51 +0700

    goal, fix physic

--- Assets/00_BaseGame/00_Script/00_Controller/GamePlayController/Level/ItemPlacer.cs
+++ Assets/00_BaseGame/00_Script/00_Controller/GamePlayController/Level/ItemPlacer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -8,7 +9,7 @@ using UnityEngine;
 public struct ItemLevelData
 {
     public Item itemPrefab;
-    
+    public bool isGoal;
     [PropertyRange(3, 99)]
     [OnValueChanged("SnapToMultipleOfThree")]
     public int amount;
@@ -24,4 +25,54 @@ public class ItemPlacer : MonoBehaviour
 {
     [Header("Elemenet")]
     [SerializeField] private List<ItemLevelData> lsItemDatas = new();
+    [SerializeField] private BoxCollider spawnZone;
+    [SerializeField] private int seed;
+    
+    
+    public ItemLevelData[] GetGoal()
+    {
+        List<ItemLevelData> goals = new List<ItemLevelData>();
+        foreach (var data in this.lsItemDatas)
+        {
+            if(data.isGoal)
+                goals.Add(data);
+        }
+        return goals.ToArray();
+    }
+    
+    
+    [Button("Generate Item", ButtonSizes.Large)]
+    private void GenerateItem()
+    {
+        while (transform.childCount > 0)
+        {
+            var t = transform.GetChild(0);
+            t.SetParent(null);
+            DestroyImmediate(t.gameObject);
+        }
+        Random.InitState(seed);
+        for (int i = 0; i < lsItemDatas.Count; i++)
+        {
+            var itemDataClone =  lsItemDatas[i];
+            for (int j = 0; j < itemDataClone.amount; j++)
+            {
+                Vector3 spawnPosition = GetSpawnPosition();
+                Item itemInstance = PrefabUtility.InstantiatePrefab(itemDataClone.itemPrefab,transform) as Item;
+                itemInstance.transform.position = spawnPosition;
+                itemInstance.transform.rotation = Quaternion.Euler(Random.onUnitSphere * 360);
+            }
+        }
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        float x = Random.Range(-spawnZone.size.x / 2, spawnZone.size.x / 2);
+        float y = Random.Range(-spawnZone.size.y / 2, spawnZone.size.y / 2);
+        float z = Random.Range(-spawnZone.size.z / 2, spawnZone.size.z / 2);
+        
+        var localPosition = spawnZone.center + new Vector3(x, y, z);
+        var spawnPosition = transform.TransformPoint(localPosition);
+        return spawnPosition;
+    }
+
 }
